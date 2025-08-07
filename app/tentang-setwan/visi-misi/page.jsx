5c3381cde03ba83c443f87bc000f08557de6f85a
bugs macbook-air <rnaldi491@gmail.com>
2025-08-08 06:49:16 +0900

    add a couples model

--- app/tentang-setwan/visi-misi/page.jsx
+++ app/tentang-setwan/visi-misi/page.jsx
@@ -17,28 +17,28 @@ const VisiMisiPage = () => {
             <p className="text-white text-left">
                 Guna mewujutkan kondisi dalam visi, maka akan diwujutkan misi sebagai berikut:
             </p>
-            <ol type="a" class="list-[lower-alpha] pl-6 space-y-4 text-white lg:pl-20">
+            <ol type="a" className="list-[lower-alpha] pl-6 space-y-4 text-white lg:pl-20">
                 <li>
-                    <p class="font-semibold">Meningkatkan kualitas Pelayanan Dan Kinerja Aparatur Dalam Aspek Teknis Maupun Administratif.</p>
-                    <p class="mt-1">
+                    <p className="font-semibold">Meningkatkan kualitas Pelayanan Dan Kinerja Aparatur Dalam Aspek Teknis Maupun Administratif.</p>
+                    <p className="mt-1">
                     Meningkatkan kualitas pelayanan dan kinerja aparatur dalam aspek teknis maupun administratif, mengandung makna bahwa dalam rangka untuk mendukung atau menunjang terlaksananya pelayanan terhadap fungsi dan kewenangan Legislatif, maka harus tersedia kualitas pelayanan administrasi maupun kinerja aparatur yang baik sehingga fungsi dan kewenangan Legislatif dapat terwujud sesuai dengan yang diharapkan.
                     </p>
                 </li>
                 <li>
-                    <p class="font-semibold">Meningkatkan Penyediaan Fasilitas Pelayanan Guna Menunjang Fungsi dan Tugas Legislatif.</p>
-                    <p class="mt-1">
+                    <p className="font-semibold">Meningkatkan Penyediaan Fasilitas Pelayanan Guna Menunjang Fungsi dan Tugas Legislatif.</p>
+                    <p className="mt-1">
                     Meningkatkan Penyediaan fasilitas Pelayanan Guna Menunjang fungsi dan tugas legislatif, mengandung makna bahwa dalam rangka untuk mendukung atau menunjang terlaksananya pelayanan prima maka diperlukan fasilitas baik sarana maupun prasarana yang mendukung terwujudnya fungsi dan tugas legislatif.
                     </p>
                 </li>
                 <li>
-                    <p class="font-semibold">Meningkatkan Sumber Daya Aparatur Dalam Menunjang Fungsi dan kewenangan Legislatif.</p>
-                    <p class="mt-1">
+                    <p className="font-semibold">Meningkatkan Sumber Daya Aparatur Dalam Menunjang Fungsi dan kewenangan Legislatif.</p>
+                    <p className="mt-1">
                     Meningkatkan sumber daya aparatur dalam menunjang fungsi dan kewenangan legislatif, mengandung makna bahwa dalam rangka meningkatkan sumber daya aparatur maka diperlukan pelatihan/bimbingan teknis tentang perundang-undangan sehingga mendukung terwujudnya fungsi dan tugas legislatif.
                     </p>
                 </li>
                 <li>
-                    <p class="font-semibold">Meningkatkan Kapasitas Lembaga Perwakilan Rakyat Daerah.</p>
-                    <p class="mt-1">
+                    <p className="font-semibold">Meningkatkan Kapasitas Lembaga Perwakilan Rakyat Daerah.</p>
+                    <p className="mt-1">
                     Meningkatkan Kapasitas Lembaga Perwakilan Rakyat Daerah, Mengandung makna bahwa dalam rangka untuk memperkuat peran politik lembaga perwakilan rakyat daerah.
                     </p>
                 </li>
