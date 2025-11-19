7013b69cf97079461645371f8f5b24251b738533
bugs macbook-air <rnaldi491@gmail.com>
2025-11-20 05:03:42 +0900

    uncoment volume setup on compose file 🥱

--- app/kontak/page.jsx
+++ app/kontak/page.jsx
@@ -12,9 +12,9 @@ const KontakPage = async () => {
         <HeaderPages title={"Kontak"} />
         <div className='flex flex-col gap-2 items-center justify-center w-full lg:p-20 mb-10 lg:mb-0'>
             <p className='lg:font-semibold lg:text-lg text-base text-white'>Alamat Sekretariat DPRK Waropen :</p>
-            <p className='font-mono lg:text-lg text-base text-white'>{kontak.alamat}</p>
-            <p className='font-mono lg:text-lg text-base text-white'>Telp. {kontak.telp}</p>
-            <p className='font-mono lg:text-lg text-sm text-white'>Email : <a className='text-blue-400' href={`mailto:${kontak.email}`}>{kontak.email}</a></p>
+            <p className='font-mono lg:text-lg text-base text-white'>{kontak?.alamat || "-"}</p>
+            <p className='font-mono lg:text-lg text-base text-white'>Telp. {kontak?.telp || "-"}</p>
+            <p className='font-mono lg:text-lg text-sm text-white'>Email : <a className='text-blue-400' href={`mailto:${kontak?.email || "#"}`}>{kontak?.email || "-"}</a></p>
             <p className='font-mono lg:text-lg text-sm text-white'>Website : <a className='text-blue-400' target='_blank' href={host}>{host}</a></p>
         </div>
     </>
