902a8c992ccab4e75dced56f1a646da8fd5f8120
bugs macbook-air <rnaldi491@gmail.com>
2026-09-23 10:31:06 +0900

    add trycatch

--- components/custom/kalender-section.jsx
+++ components/custom/kalender-section.jsx
@@ -6,7 +6,13 @@ import Calendar from './kalender-component'
 
 const KalenderSection = async () => {
 
-  const data = await getAgendaList()
+  let data = []
+  try {
+    data = await getAgendaList()
+  } catch (error) {
+  }
+
+
 
   return (
     <div className="relative overflow-visible z-0 bg-[radial-gradient(#312e31_1px,transparent_0.3px)] [background-size:16px_16px] w-full pb-20 bg-[#231c26] px-10 lg:px-20">
